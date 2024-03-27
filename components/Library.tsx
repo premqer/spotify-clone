ca10609cad7e31dca7e5a0d2e57123a77b4ba6ec
Vlad <vladm19082006@gmail.com>
2024-03-27 19:09:19 +0300

    Still working on the layout

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from  "react-icons/ai";
 
 const Library = () => {
     const onClick = () => {
@@ -34,6 +35,25 @@ const Library = () => {
                         Your Library
                     </p>
                 </div>
+                <AiOutlinePlus
+                    onClick={onClick}
+                    size={20}
+                    className='
+                        text-neutral-400
+                        cursor-pointer
+                        hover:text-white
+                        transition
+                    '
+                />
+            </div>
+            <div className='
+                flex
+                flex-col
+                gap-y-2
+                mt-4
+                px-3
+            '>
+                List of Songs
             </div>
         </div>
      );
