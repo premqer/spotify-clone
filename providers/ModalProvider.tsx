6acf005186351194f081d384905375010518413d
Vlad <vladm19082006@gmail.com>
2024-10-06 21:18:05 +0200

    Add songs. Input when adding songs

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 
 import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
 
 const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -20,6 +21,7 @@ const ModalProvider = () => {
     return ( 
         <>
             <AuthModal />
+            <UploadModal />
         </>
      );
 }
