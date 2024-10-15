a0ba4e7a690894f1c823d309739447a7ec0b6253
Vlad <vladm19082006@gmail.com>
2024-10-15 22:02:41 +0200

    List display

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,14 @@
+
+import getSongs from '@/actions/getSongs';
 import Header from '@/components/Header';
 import ListItem from '@/components/ListItem';
 
-export default function Home() {
+import PageContent from './components/PageContent';
+
+export const revalidate = 0;
+
+export default async function Home() {
+  const songs = await getSongs();
   return (
     <div className='
       bg-neutral-900
@@ -47,9 +54,7 @@ export default function Home() {
             Newest songs
           </h1>
         </div>
-        <div>
-          List of songs
-        </div>
+        <PageContent songs={songs} />
       </div>
     </div>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            "iojfeqnithcrqsffxwpv.supabase.co"
+        ]
+    }
+};
 
 export default nextConfig;
